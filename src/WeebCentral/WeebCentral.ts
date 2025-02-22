0e6e4bf1c4ea821588337e93a571ea65e5dbbd33
Plancktron <147928761+Plancktron@users.noreply.github.com>
2025-02-22 19:19:20 +0800

    Update WeebCentral.ts

--- src/WeebCentral/WeebCentral.ts
+++ src/WeebCentral/WeebCentral.ts
@@ -1,67 +1,189 @@
-import { Source, Manga, MangaStatus, Chapter, ChapterDetails, HomeSectionRequest, HomeSection, MangaTile, SearchRequest, LanguageCode, TagSection, Request, MangaUpdates, SourceTag, TagType, PagedResults } from "paperback-extensions-common"
-const SOURCE_DOMAIN = "https://weebcentral.com"
+import {
+    SourceManga,
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    SearchRequest,
+    PagedResults,
+    SourceInfo,
+    TagSection,
+    ContentRating,
+    Request,
+    Response,
+    SourceIntents,
+    SearchResultsProviding,
+    ChapterProviding,
+    MangaProviding,
+    HomePageSectionsProviding,
+    Tag
+} from '@paperback/types'
 
-export class WeebCentral extends Source {
-    constructor(cheerio: CheerioAPI) {
-        super(cheerio)
+import * as cheerio from 'cheerio'
+
+import {
+    isLastPage,
+    parseTags,
+    parseChapterDetails,
+    parseChapters,
+    parseHomeSections,
+    parseMangaDetails,
+    parseSearch,
+    parseViewMore
+} from './WeebCentralParser'
+
+const WC_DOMAIN = 'https://www.weebcentral.com'
+
+export const WeebCentralInfo: SourceInfo = {
+    version: '3.0.5',
+    name: 'WeebCentral',
+    icon: 'icon.jpeg',
+    author: 'Plancktron',
+    authorWebsite: 'https://github.com/Plancktron',
+    description: 'Extension that pulls manga from weebcentral.com',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: WC_DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED
+}
+
+export class WeebCentral implements SearchResultsProviding, MangaProviding, ChapterProviding, HomePageSectionsProviding {
+
+    requestManager = App.createRequestManager({
+        requestsPerSecond: 10,
+        requestTimeout: 20000,
+        interceptor: {
+            interceptRequest: async (request: Request): Promise<Request> => {
+                request.headers = {
+                    ...(request.headers ?? {}),
+                    ...{
+                        'referer': `${WC_DOMAIN}/`,
+                        'user-agent': await this.requestManager.getDefaultUserAgent()
+                    }
+                }, request.cookies = [
+                    App.createCookie({ name: 'isAdult', value: '1', domain: 'www.weebcentral.com' })
+                ]
+                return request
+            },
+            interceptResponse: async (response: Response): Promise<Response> => {
+                return response
+            }
+        }
+    });
+
+    getMangaShareUrl(mangaId: string): string { return `${WC_DOMAIN}/manga/${mangaId}` }
+
+    async getMangaDetails(mangaId: string): Promise<SourceManga> {
+        const request = App.createRequest({
+            url: `${WC_DOMAIN}/manga/${mangaId}`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        return parseMangaDetails($, mangaId)
     }
 
-    get version(): string { return '0.1.0' }
-    get name(): string { return 'WeebCentral' }
-    get description(): string { return 'A source containing manga from WeebCentral, the descendant of Manga4Life and MangaSee' }
-    get author(): string { return 'Plancktron' }
-    get authorWebsite(): string { return '' }
-    get icon(): string { return "logo.png" }
-    get hentaiSource(): boolean { return false }
-    get sourceTags(): SourceTag[] { return [] }
-    get rateLimit(): Number {
-      return 2
-    }
-    get websiteBaseURL(): string { return SOURCE_DOMAIN }
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        const request = App.createRequest({
+            url: `${WC_DOMAIN}/manga/${mangaId}`,
+            method: 'GET'
+        })
 
-    /**
-     * This method has been provided to you as it's probably rather rare that it ever needs to be changed
-     * even on different sources.
-     * This Method should point to the URL of a specific manga object on your source. Make sure to change
-     * line 33 so that it points to your manga! In this case, 'id' is the manga identifier
-     */
-    getMangaDetailsRequest(ids: string[]): Request[] {
-      let requests: Request[] = []
-      for (let id of ids) {
-        let metadata = { 'id': id }
-        requests.push(createRequestObject({
-          url: `${SOURCE_DOMAIN}/${id}`,
-          metadata: metadata,
-          method: 'GET'
-        }))
-      }
-      return requests
-    }
-    
-    getMangaDetails(data: any, metadata: any): Manga[] {
-      let manga: Manga[] = []                           // For your getMangaDetailsRequest URL request, fill this object out with each manga
-      let $ = this.cheerio.load(data)                   // CheerioJS has been loaded with the HTML response, use this to fill out your objects
-
-      // manga.push(createManga({...}))                 For each object, ALWAYS wrap it in the create tag. createManga({..}), createIconText({...}), etc
-      throw new Error("Method not implemented.")
-    }
-    getChaptersRequest(mangaId: string): Request {
-      throw new Error("Method not implemented.")
-    }
-    getChapters(data: any, metadata: any): Chapter[] {
-      throw new Error("Method not implemented.")
-    }
-    getChapterDetailsRequest(mangaId: string, chapId: string): Request {
-      throw new Error("Method not implemented.")
-    }
-    getChapterDetails(data: any, metadata: any): ChapterDetails {
-      throw new Error("Method not implemented.")
-    }
-    searchRequest(query: SearchRequest): Request {
-      throw new Error("Method not implemented.")
-    }
-    search(data: any, metadata: any): PagedResults {
-      throw new Error("Method not implemented.")
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        return parseChapters($, mangaId)
     }
 
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        const request = App.createRequest({
+            url: `${WC_DOMAIN}/manga/${mangaId}/${chapterId}/1.html`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        return parseChapterDetails($, mangaId, chapterId, request.url, this)
+    }
+
+    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const request = App.createRequest({
+            url: WC_DOMAIN,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        parseHomeSections($, sectionCallback)
+    }
+
+    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1
+        let param = ''
+
+        switch (homepageSectionId) {
+            case 'hot_release':
+                param = 'hot'
+                break
+            case 'new_manga':
+                param = `directory/${page}.htm?news`
+                break
+            case 'latest_updates':
+                param = `latest/${page}`
+                break
+            default:
+                throw new Error(`Invalid homeSectionId | ${homepageSectionId}`)
+        }
+
+        const request = App.createRequest({
+            url: `${WC_DOMAIN}/${param}`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        const manga = parseViewMore($, homepageSectionId)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1
+
+        const url = new URLBuilder(WC_DOMAIN)
+            .addPathComponent('search')
+            .addQueryParameter('page', page)
+            .addQueryParameter('title', encodeURI(query?.title || ''))
+            .addQueryParameter('genres', query.includedTags?.map((x: Tag) => x.id).join('%2C'))
+            .buildUrl()
+
+        const request = App.createRequest({
+            url: url,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        const manga = parseSearch($)
+
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+        return App.createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+
+    async getSearchTags(): Promise<TagSection[]> {
+        const request = App.createRequest({
+            url: `${WC_DOMAIN}/search?`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = cheerio.load(response.data as string)
+        return parseTags($)
+    }
 }
